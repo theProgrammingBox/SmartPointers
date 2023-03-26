ee7389995387715a677d674db409146bc6f1c766
theProgrammingBox <justinboxgea@gmail.com>
2023-03-25 17:05:14 -0700

    more testing

--- SmartPointers/Source.cpp
+++ SmartPointers/Source.cpp
@@ -30,7 +30,7 @@ int main()
 	std::cout << *two << std::endl;
 	std::cout << std::endl;
 
-	one = std::move(two);	// address replaced, not value. two is now empty
+	one = std::move(two);	// address replaced. two is now empty
 	
 	std::cout << one << std::endl;
 	std::cout << *one << std::endl;
@@ -39,7 +39,21 @@ int main()
 	std::cout << std::endl;
 	std::cout << std::endl;
 	
-	two = std::make_unique<uint32_t>(2);
+	uint32_t* temp = one.get();
+	std::cout << one << std::endl;
+	std::cout << "--------------" << temp << std::endl;
+	
+	two = std::make_unique<uint32_t>(2);	// address replaced. two is 2
+	one = std::make_unique<uint32_t>(1);	// address replaced. one is 1
+
+	std::cout << one << std::endl;
+	std::cout << "--------------" << temp << std::endl;	// temp address is not updated
+	
+	std::cout << one << std::endl;
+	std::cout << *one << std::endl;
+	std::cout << two << std::endl;
+	std::cout << *two << std::endl;
+	std::cout << std::endl;
 	
 	return 0;
 }
